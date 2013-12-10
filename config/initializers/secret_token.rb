3323f302f19e78a1d1a1e979ddb484c4fbf60f4e
maloneyl <m@thinkmaloney.com>
2013-12-10 15:30:06 +0000

    created rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeamStatusApp::Application.config.secret_token = '55629090dcc0dca9390bd4bc015c9b412970fbcb1067063abd45bd0a92f7650d6461c47ee38f617dedd9012283f413044c73e5fadb84960ea73b6cee8878d288'
