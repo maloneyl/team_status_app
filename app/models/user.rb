4412446842554e83be6f51ec8053900800b0362f
maloneyl <m@thinkmaloney.com>
2013-12-10 18:55:46 +0000

    added details to users: first name, last name, username, image, role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ActiveRecord::Base
 
   # Others available are: :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  # will need to add to model: name, handle
+  attr_accessible :email, :first_name, :last_name, :username, :password, :password_confirmation, :remember_me
+
 
   has_and_belongs_to_many :groups
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131210171227) do
+ActiveRecord::Schema.define(:version => 20131210184346) do
 
   create_table "groups", :force => true do |t|
     t.string   "name"
@@ -60,6 +60,12 @@ ActiveRecord::Schema.define(:version => 20131210171227) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
+    t.string   "role"
+    t.text     "image"
+    t.string   "bio"
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
