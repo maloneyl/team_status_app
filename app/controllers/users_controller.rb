bae026085ec0e03be9ef6df3a741f98f7e74f968
maloneyl <m@thinkmaloney.com>
2013-12-12 17:55:34 +0000

    updated users show view with partials and authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,11 +12,13 @@ class UsersController < Devise::RegistrationsController
   def show
     @user = User.find params[:id]
 
-    @mentions = []
-    Status.with_mentions.each do |status|
-      @mentions << status if status.at_user == current_user.username || status.at_user == "all"
+    if user_signed_in?
+      @mentions = []
+      Status.with_mentions.each do |status|
+        @mentions << status if status.at_user == current_user.username || status.at_user == "all"
+      end
+      @mentions = @mentions.sort_by(&:created_at).reverse
     end
-    @mentions = @mentions.sort_by(&:created_at).reverse
   end
 
   def update
