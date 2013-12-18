81cbb5fdc5411c7dcff42821a47cc5919aa1dfdb
maloneyl <m@thinkmaloney.com>
2013-12-18 15:23:04 +0000

    fixed 'yesterday' typo and got user dashboard charts into pie charts split by durations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,39 +9,40 @@ class UsersController < Devise::RegistrationsController
   def show
     @user = User.find params[:id]
 
-    if user_signed_in?
-      @mentions = []
-      @groups = @user.groups
-      @groups.each do |group|
-        group.statuses.with_mentions.each do |status|
-          @mentions << status if status.at_user == current_user.username || status.at_user == "all"
-        end
+    # get groups and mentions
+    @mentions = []
+    @groups = @user.groups
+    @groups.each do |group|
+      group.statuses.with_mentions.each do |status|
+        @mentions << status if status.at_user == current_user.username || status.at_user == "all"
       end
-      @mentions = @mentions.sort_by(&:created_at).reverse
+    end
+    @mentions = @mentions.sort_by(&:created_at).reverse
 
-      @agendas = @user.agendas
+    # get agendas
+    @agendas = @user.agendas.all(:order => 'updated_at DESC')
 
-      @items_today = @user.statuses.trackable.today rescue nil
-      @items_yesteday = @user.statuses.trackable.yesterday rescue nil
+    # get items for reports
+    @items_today = @user.statuses.trackable.today rescue nil
+    @items_yesterday = @user.statuses.trackable.yesterday rescue nil
 
-      # update duration if tracking
-      if @items_today.present?
-        @items_today.each do |status|
-          if status.tracking?
-            if status.durations.last.present?
-              status.time_tracked = status.durations.today.sum(:time_elapsed) + (Time.now - status.durations.today.last.updated_at).to_i
-            else
-              status.time_tracked = (Time.now - status.durations.today.last.updated_at).to_i
-            end
-            status.save!
+    # update item duration if tracking
+    if @items_today.present?
+      @items_today.each do |status|
+        if status.tracking?
+          if status.durations.last.present?
+            status.time_tracked = status.durations.today.sum(:time_elapsed) + (Time.now - status.durations.today.last.updated_at).to_i
+          else
+            status.time_tracked = (Time.now - status.durations.today.last.updated_at).to_i
           end
+          status.save!
         end
       end
+    end
 
-      if @items_yesterday.present?
-        @items_yesterday.each do |status|
-          status.time_tracked = status.durations.yesterday.sum(:time_elapsed)
-        end
+    if @items_yesterday.present?
+      @items_yesterday.each do |status|
+        status.time_tracked = status.durations.yesterday.sum(:time_elapsed)
       end
     end
   end
