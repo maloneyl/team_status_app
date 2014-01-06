1cf653399f52b68a8ccd74664a3f2cd137582b62
maloneyl <m@thinkmaloney.com>
2014-01-06 18:08:35 +0000

    updated seed data to fit demo use case

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,24 +10,36 @@ User.delete_all
 Group.delete_all
 Status.delete_all
 
-u1 = User.create!(:email => 'maloney.liu@gmail.com', :first_name => 'Maloney', :last_name => 'Liu', :username => 'maloney', :password => 'maloney', :password_confirmation => 'maloney')
-u1.confirmed_at = u1.created_at + 2.seconds
+u1 = User.create!(:email => 'maloney@maloney.com', :first_name => 'Maloney', :last_name => 'Liu', :username => 'maloney', :password => 'maloney', :password_confirmation => 'maloney')
+# u1.confirmed_at = u1.created_at + 2.seconds
 
-u2 = User.create!(:email => 'm@thinkmaloney.com', :first_name => 'M', :last_name => 'Baloney', :username => 'missbaloney', :password => 'maloney', :password_confirmation => 'maloney')
-u2.confirmed_at = u2.created_at + 2.seconds
+u2 = User.create!(:email => 'belle@belle.com', :first_name => 'Belle', :last_name => 'Beans', :username => 'belle', :password => 'belle', :password_confirmation => 'belle')
+# u2.confirmed_at = u2.created_at + 2.seconds
 
-u3 = User.create!(:email => 'thinkmaloney@gmail.com', :first_name => 'Think', :last_name => 'Maloney', :username => 'thinkmaloney', :password => 'maloney', :password_confirmation => 'maloney')
-u3.confirmed_at = u3.created_at + 3.seconds
+u3 = User.create!(:email => 'wouter@wouter.com', :first_name => 'Wouter', :last_name => 'DL', :username => 'wouter', :password => 'wouter', :password_confirmation => 'wouter')
+# u3.confirmed_at = u3.created_at + 3.seconds
 
-g1 = Group.create!(:name => 'Test Group 1', :owner_id => u1.id)
+u4 = User.create!(:email => 'mitchell@mitchell.com', :first_name => 'Mitchell', :last_name => 'Pritchett', :username => 'mitchell', :password => 'mitchell', :password_confirmation => 'mitchell')
+
+u5 = User.create!(:email => 'cameron@cameron.com', :first_name => 'Cameron', :last_name => 'Tucker', :username => 'cameron', :password => 'cameron', :password_confirmation => 'cameron')
+
+u6 = User.create!(:email => 'john@john.com', :first_name => 'John', :last_name => 'Exo', :username => 'john', :password => 'john', :password_confirmation => 'john')
+
+g1 = Group.create!(:name => 'Beans Group', :owner_id => u2.id)
 g1.users << u1
-g1.users << u2
+g1.users << u3
 
-g2 = Group.create!(:name => 'Test Group 2', :owner_id => u2.id)
-g2.users << u2
-g2.users << u3
+g2 = Group.create!(:name => 'Team Modern', :owner_id => u1.id)
+g2.users << u4
+g2.users << u5
 
-g3 = Group.create!(:name => 'Test Group 3', :owner_id => u3.id)
-g3.users << u3
-g3.users << u1
-g3.users << u2
+#g1
+s1 = Status.create!(:body => "@all I need to leave a couple hours early today so grab me earlier if you need me!", :tracking => false, :tracked => false, :user_id: u2.id, :group_id => g1.id)
+s2 = Status.create!(:body => "@belle Most likely won't have wireframes done until EOD. Will just Basecamp you then.", :tracking => false, :tracked => false, :user_id: u1.id, :group_id => g1.id)
+s3 = Status.create!(:body => "Stepping out for a bit.", :tracking => false, :tracked => false, :user_id: u3.id, :group_id => g1.id)
+s4 = Status.create!(:body => "@maloney LPH staging is up - could you check the copy when you get the chance?", :tracking => false, :tracked => false, :user_id: u6.id, :group_id => g1.id)
+a1 = Agenda.create!(:body => "Launch LPH", :group_id: g1.id, :user_id: u6.id)
+a2 = Agenda.create!(:body => "Research and write proposal for XYZ", :group_id: g1.id, :user_id: u6.id)
+
+# g2
+s5 = Status.create!(:body => "@maloney Thanks for setting up the new group!", :tracking => false, :tracked => false, :user_id: u4.id, :group_id => g2.id)
