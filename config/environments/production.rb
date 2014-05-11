dd06fec31546f15412481f6ec57c03bc80e885ff
maloneyl <m@thinkmaloney.com>
2014-05-11 11:42:30 +0100

    vendor compiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,9 @@ TeamStatusApp::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
+  # Modernizr
+  config.assets.precompile += %w( vendor/modernizr.js )
+
   # Generate digests for assets URLs
   config.assets.digest = true
 
